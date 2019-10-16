25b83661dc862240a707d3f713285a69fb3b9079
Bryan Nishi <Bryan.Nishi@gmail.com>
2019-10-16 12:33:53 -0600

    CORS for Heroku
    testing

--- client/src/services/PostData.js
+++ client/src/services/PostData.js
@@ -1,5 +1,5 @@
     export function PostData(type, userData) {
-        let BaseURL = 'http://localhost:3000//api/user';
+        let BaseURL = 'http://localhost:3000/api/user';
         //let BaseURL = 'http://localhost/socialapi/';
     
         return new Promise((resolve, reject) =>{
